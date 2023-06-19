8fccd692afabdf7670db030bfc45955505aa0937
saurabhla <69839666+saurabhla@users.noreply.github.com>
2023-06-19 11:16:27 +0530

    Update streamlit_app.py
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My New Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text(' Omega 3 & Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
