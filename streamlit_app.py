7fba751d1509750c39aef73991cdb9398da67dff
saurabhla <69839666+saurabhla@users.noreply.github.com>
2023-06-19 11:20:51 +0530

    Update streamlit_app.py
    Added icons and new header line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My New Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text(' Omega 3 & Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
