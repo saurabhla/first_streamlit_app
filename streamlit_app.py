e17351d57492bf2158bb4d728dc0017f933c2c46
saurabhla <69839666+saurabhla@users.noreply.github.com>
2023-06-21 16:02:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 import requests
 streamlit.title('My New Diner')
 streamlit.header('Breakfast Menu')
-
+streamlit.text('🥣Omega 3 & Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
