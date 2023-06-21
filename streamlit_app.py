57a6d2d2fc4700d961fa4f252fe84b9e350dee83
saurabhla <69839666+saurabhla@users.noreply.github.com>
2023-06-21 15:57:25 +0530

    Update streamlit_app.py
    added requests library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.title('My New Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Oatmeal')
@@ -12,3 +13,5 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
